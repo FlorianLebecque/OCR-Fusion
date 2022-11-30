71278f03470e663a03f460c387bafe03bab1ebbc
Florian <Florianlebecque@hotmail.com>
2022-11-30 11:02:52 +0100

    Base multiplex

--- OCR Fusion/Multiplex.cs
+++ OCR Fusion/Multiplex.cs
@@ -1,4 +1,18 @@
-﻿namespace OCR_Fusion {
+﻿using OCR_Fusion.OCR.Typography.Iron;
+
+namespace OCR_Fusion {
     public class Multiplex {
+    
+    
+        public static IOCRManager GetOCR(bool isHandWritter) {
+
+            if (isHandWritter) {
+                return new IronOCR();
+            }
+
+            throw new NotImplementedException();
+
+        }
+    
     }
 }
