f780648ee4a81d607a26197dc683b8ada62e0dc2
Florian <Florianlebecque@hotmail.com>
2022-11-30 15:03:51 +0100

    Implement simple OCR Controller

--- OCR Fusion/OCRController.cs
+++ OCR Fusion/OCRController.cs
@@ -1,4 +1,18 @@
 ﻿namespace OCR_Fusion {
     public class OCRController {
+
+        private IOCRManager ocrManager;
+        public OCRController(IOCRManager oCRManager){ 
+            this.ocrManager = oCRManager;
+        }
+
+
+        public OutputDefinition GetText(InputDefinition input) {
+
+            OutputDefinition output = ocrManager.GetText(input);
+
+            return output;
+        }
+
     }
 }
