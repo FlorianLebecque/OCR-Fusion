70b9907212cb64c9938a062d2c517c46d479479f
Florian <Florianlebecque@hotmail.com>
2022-11-30 10:54:10 +0100

    Add post image and Gettext route

--- OCR Fusion/Controllers/OcrController.cs
+++ OCR Fusion/Controllers/OcrController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+
 
 namespace OCR_Fusion.Controllers {
 
@@ -7,12 +10,46 @@ namespace OCR_Fusion.Controllers {
     [Route("[controller]")]
     public class OcrController : ControllerBase {
 
+
+        private IWebHostEnvironment Environment;
+        private string uploadPath;
+
+        public OcrController(IWebHostEnvironment _environment) {
+            Environment = _environment;
+
+            //uploadPath = Path.Combine(Environment.WebRootPath, "Uploads/");
+            uploadPath = "Uploads/";
+
+        }
+
+
         [HttpGet(Name = "GetText")]
         public OutputDefinition Get(InputDefinition input) {
 
-                
+            
 
             return new OutputDefinition();
+        }
+
+
+        [HttpPost]
+        public string PostImage(IFormFile file) {
+
+            if (!Directory.Exists(uploadPath)) {
+                Directory.CreateDirectory(uploadPath);
+            }
+
+            if (file.Length > 0) {
+
+                string filename = Path.Combine(uploadPath, file.FileName);
+
+                using (var fileStream = new FileStream(filename, FileMode.Create)) {
+                    file.CopyToAsync(fileStream);
+                }
+                return filename;
+            }
+
+            return "0";
         }
     }
 }
