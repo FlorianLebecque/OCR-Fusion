08b56cd15a178120a25657ff7e81badc7e7ca8a7
Florian <Florianlebecque@hotmail.com>
2022-11-30 15:02:50 +0100

    Check if file existe

--- OCR Fusion/Controllers/ImageController.cs
+++ OCR Fusion/Controllers/ImageController.cs
@@ -9,7 +9,13 @@ namespace OCR_Fusion.Controllers {
         [HttpGet(Name = "{filename}")]
         public IActionResult GetImg(string filename) {
 
-            return File(System.IO.File.ReadAllBytes("Uploads/" + filename), "image/jpeg");
+            filename = "Uploads/" + filename;
+
+            if (!System.IO.File.Exists(filename)) {
+                throw new Exception("File not found");
+            }
+
+            return File(System.IO.File.ReadAllBytes(filename), "image/"+Utils.GetExtention(filename));
         }
     }
 }
