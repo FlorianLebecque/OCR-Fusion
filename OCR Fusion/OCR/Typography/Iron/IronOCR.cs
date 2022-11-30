85c617e3fef8233e6781d03dd7e8fc1a240dc52f
Louis-Antoine DEVOS <18168@ecam.be>
2022-11-30 23:06:39 +0100

    Add IronOCR

--- OCR Fusion/OCR/Typography/Iron/IronOCR.cs
+++ OCR Fusion/OCR/Typography/Iron/IronOCR.cs
@@ -1,7 +1,23 @@
 ﻿namespace OCR_Fusion.OCR.Typography.Iron {
+    using IronOcr;
     public class IronOCR : IOCRManager {
         public OutputDefinition GetText(InputDefinition input) {
+            IronText();
             throw new NotImplementedException();
+        }
+
+        private List<string> IronText()
+        {
+            List<string> text = new List<string>();
+            var Ocr = new IronTesseract();
+            using (var Input = new OcrInput())
+            {
+                Input.AddImage(@"OCR_Test.png");
+                var Result = Ocr.Read(Input);
+                Console.WriteLine(Result.Text);
+                text.Add(Result.Text);
+            }
+            return text;
         }
     }
 }
