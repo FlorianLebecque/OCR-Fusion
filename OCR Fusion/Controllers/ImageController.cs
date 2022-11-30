23598ecab9a060862de270bc3ac793e2f7a64d3d
Florian <Florianlebecque@hotmail.com>
2022-11-30 10:53:52 +0100

    Add Image controller to retrieve image from server

---
+++ OCR Fusion/Controllers/ImageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace OCR_Fusion.Controllers {
+
+    [Route("[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase {
+        [HttpGet(Name = "{filename}")]
+        public IActionResult GetImg(string filename) {
+
+            return File(System.IO.File.ReadAllBytes("Uploads/" + filename), "image/jpeg");
+        }
+    }
+}
