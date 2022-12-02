736dcc3b0f3245c4e332f6f1fcf2fa1b5caa75b0
Lebecque Florian <florianlebecque@hotmail.com>
2022-12-02 14:04:48 +0100

    Use config to send correct ocr

--- OCR Fusion/Multiplex.cs
+++ OCR Fusion/Multiplex.cs
@@ -1,12 +1,18 @@
 ﻿using OCR_Fusion.OCR.Typography.Iron;
+using OCR_Fusion.OCR.Typography.Placeholder;
 
 namespace OCR_Fusion {
     public class Multiplex {
     
     
-        public static IOCRManager GetOCR(bool isHandWritter) {
+        public static IOCRManager GetOCR(InputDefinition input,IConfiguration config) {
 
-            if (isHandWritter) {
+            if (config.GetValue<string>("mode") == "test") {
+                return new PlaceholderOCR();
+            }
+
+
+            if (!input.IsHandWritten) {
                 return new IronOCR();
             }
 
