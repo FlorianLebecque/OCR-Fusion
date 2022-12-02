dc99e0884dd7fedc6ad1cbfa65f65037d9cf296b
Lebecque Florian <florianlebecque@hotmail.com>
2022-12-02 14:02:41 +0100

    Change name imageLink to imageName

--- OCR Fusion/API Object/OutputDefinition.cs
+++ OCR Fusion/API Object/OutputDefinition.cs
@@ -2,8 +2,8 @@
 
 namespace OCR_Fusion {
     public class OutputDefinition {
-        public string imageLink { get; set; }
+        public string imageName { get; set; }
         public List<string> words { get; set; }
-        public Dictionary<string, Vector2[]> regions { get; set; }
+        public Dictionary<string, Vector2[]>? regions { get; set; }
     }
 }
