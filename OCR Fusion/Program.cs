e00fcca8ca1abcfe8c5feff4364e655c895e600d
Florian <Florianlebecque@hotmail.com>
2022-12-08 15:02:44 +0100

    Comment initialisation for mongoDB

--- OCR Fusion/Program.cs
+++ OCR Fusion/Program.cs
@@ -1,3 +1,6 @@
+using OCR_Fusion;
+using OCR_Fusion.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//MangoCRUD db = new("test");
+//Utils.SetDatabaseInterface(db);
+
+
 
 var app = builder.Build();
 
