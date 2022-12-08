335e624bbe6ac65a8dd5b644bcc979ef38ff7a8b
Florian <Florianlebecque@hotmail.com>
2022-12-08 15:02:13 +0100

    Add CRUD database interfaces

---
+++ OCR Fusion/IDBCrud.cs
@@ -0,0 +1,10 @@
+﻿namespace OCR_Fusion {
+    public interface IDBCrud {
+
+        public void Insert<T>(string table, T value);
+        public void Update<T>(string table, T value);
+        public List<T> Gets<T>(string table,string session);
+        public void Delete<T>(string table,string session);
+
+    }
+}
