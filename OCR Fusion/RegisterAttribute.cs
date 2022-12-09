e904aa3658083218b1f610901c7fb37c02bbf530
Lebecque Florian <florianlebecque@hotmail.com>
2022-12-09 15:56:22 +0100

    Add class attributes

---
+++ OCR Fusion/RegisterAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OCR_Fusion {
+    public class RegisterAttribute : Attribute {
+
+        public string id; 
+        public string name;
+        public string description;
+        public RegisterAttribute(string id,string name,string description) {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+        }
+    }
+}
