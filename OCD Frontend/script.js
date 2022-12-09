f33e5a0056ecfc17dde6a7d210c7c67dbe9aa923
SarahLiettefti <sarah.liettefti@gmail.com>
2022-12-09 13:43:57 +0100

    fonctione plus ou moi

--- OCD Frontend/script.js
+++ OCD Frontend/script.js
@@ -3,9 +3,9 @@ let isHandWritten;
 let algorithm = [];
 let srcimg;
 
-function showPreview(event){ 
+function showPreview(event, iduplaod){ 
     if(event.target.files.length > 0){
-        var img = document.getElementById('file-ip-1');//prend le nom du premier,faire que prend le nom du deuxieme
+        var img = document.getElementById(iduplaod);//prend le nom du premier,faire que prend le nom du deuxieme
         if (img.files.item(0).type =="application/pdf"){
             alert('Convertir en jpeg png  on senfout');
         }
@@ -17,7 +17,8 @@ function showPreview(event){
       preview.src = src;
       preview.style.display = "block";
     }
-  }
+}
+
 
 document.getElementById('findtext').onclick = function() {
     isHandWritten = document.getElementById('handwritten').checked;
@@ -47,7 +48,7 @@ document.getElementById('findtext').onclick = function() {
         let bodyJSON = JSON.stringify(body);
         ShowResults();
         alert(bodyJSON);//envoie el body à l'API
-        document.getElementById("firstpreview").style.display = "none";
+        //document.getElementById("firstpreview").style.display = "none";
         document.getElementById("recherche").innerHTML = nameimg + algorithm;//fonctionne pas
     }
     return false;
@@ -55,6 +56,7 @@ document.getElementById('findtext').onclick = function() {
 }
 
 //function EnvoiePost()
+
 function ShowResults(){
     document.getElementById("preview-results").style.display = "block";
     var preview = document.getElementById("display-result-img");
@@ -63,4 +65,5 @@ function ShowResults(){
     document.getElementById("preview-is-handwritten").style.display = "block";
     document.getElementById("preview-is-printed").style.display = "block";
     document.getElementById("preview-is-other").style.display = "block";
+    return false;
 }
