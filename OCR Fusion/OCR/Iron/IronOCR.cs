ec02ac872e5a2e436de5179ae7023468cfaf473b
Lebecque Florian <florianlebecque@hotmail.com>
2022-12-16 17:14:54 +0100

    Setup parameters exemple

--- OCR Fusion/OCR/Iron/IronOCR.cs
+++ OCR Fusion/OCR/Iron/IronOCR.cs
@@ -1,6 +1,7 @@
 ﻿namespace OCR_Fusion.OCR.Iron
 {
     using IronOcr;
+    using MongoDB.Bson;
     using System.Text.Json.Nodes;
 
     [Register("IronOCR","Iron","Great recognition of printed caracters")]
@@ -11,8 +12,36 @@
 
             JsonObject parameters = new();
 
-            parameters.Add("test","gee");
-            
+            JsonObject lang = new();
+            lang.Add("type", "text");
+            lang.Add("title", "Lang");
+            lang.Add("description", "Let defined witch lang the algorithm will use");
+            lang.Add("default", "fr");
+
+            JsonObject fine = new();
+            fine.Add("type", "check");
+            fine.Add("title", "Fine");
+            fine.Add("description", "Let defined witch lang the algorithm will use");
+
+            JsonObject country = new();
+
+            Dictionary<string,string> options = new(){
+                {"fr", "French"},
+                {"nl", "Dutch"},
+                {"en", "English"} 
+            };
+
+            country.Add("type", "select");
+            country.Add("title", "Country");
+            country.Add("description", "Let defined witch lang the algorithm will use");
+            country.Add("options", options.ToJson());
+
+
+
+            parameters.Add("lang",lang);
+            parameters.Add("fine",fine);
+            parameters.Add("country", country);
+
             return parameters;
         }
 
