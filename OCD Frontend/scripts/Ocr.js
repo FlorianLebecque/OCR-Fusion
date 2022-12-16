0e26b358c631544bd52d5ed5a2eb4ca8de95a438
Lebecque Florian <florianlebecque@hotmail.com>
2022-12-16 14:29:02 +0100

    add camera logic

--- OCD Frontend/scripts/Ocr.js
+++ OCD Frontend/scripts/Ocr.js
@@ -16,7 +16,7 @@ class OcrAPI{
         let filename = this.filename;
         data.append('file', files[0]);
 
-        imageWrapper.html('<div  class="col align-self-center"><div class="lds-dual-ring"></div></div>');
+        imageWrapper.html('<div class="col align-self-center"><div class="lds-dual-ring"></div></div>');
         imageWrapper.show(100);
 
         
@@ -35,6 +35,67 @@ class OcrAPI{
             
         });
     };
+
+    async OpenCamera(){
+
+
+        let video = document.querySelector("#video");
+        let main = $("#main-wrapper");
+        let camera = $("#camera-wrapper");
+
+
+        main.hide();
+        camera.show();
+        let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false, facingMode: 'environement' });
+	    video.srcObject = stream;
+
+    }
+
+    TakePhoto(){
+        let canvas = document.querySelector("#canvas");
+        let video = document.querySelector("#video");
+        let imageWrapper = $("#imageWrapper");
+        let main = $("#main-wrapper");
+        let camera = $("#camera-wrapper");
+
+
+        canvas.width = video.offsetWidth;
+        canvas.height = video.offsetHeight;
+
+        canvas.getContext('2d').drawImage(video, 0, 0, video.offsetWidth, video.offsetHeight);
+        let image_data_url = canvas.toDataURL('image/jpeg');
+
+
+        let file = null;
+        let blob = canvas.toBlob(function(blob) {
+                file = new File([blob], 'test.jpg', { type: 'image/jpeg' });
+        }, 'image/jpeg');
+
+        let data_file = new FormData();
+        data_file.append("file",file);
+
+        main.show();
+        camera.hide();
+        imageWrapper.html('<div  class="col align-self-center"><div class="lds-dual-ring"></div></div>');
+        imageWrapper.show(100);
+
+        $.ajax({
+            url: "http://127.0.0.1:5154/Ocr",
+            type: 'put',
+            dataType: 'json',
+            data: data_file,
+            processData: false,
+            contentType: false,
+            statusCode:{
+                200: function(image){
+
+                    imageWrapper.html('<img src="http://127.0.0.1:5154/Image/'+filename+'" id="imageHolder" alt="">');
+                }
+            }
+            
+        });
+        
+    }
 
 
     RequestOCR(){
