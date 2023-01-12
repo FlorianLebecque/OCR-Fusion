001f6b417b960d96ac6022d7928fa70f121ac0bf
SarahLiettefti <sarah.liettefti@gmail.com>
2023-01-12 18:49:35 +0100

    upload pdf not working

--- OCD Frontend/scripts/Ocr.js
+++ OCD Frontend/scripts/Ocr.js
@@ -11,32 +11,78 @@ class OcrAPI{
 
         let data = new FormData(),
             files = document.getElementById("imageUpload").files;
+            console.log(typeof(files[0]));
+            //application/pdf
+            //image/jpeg
+            //image/png
 
-        this.filename = files[0].name;
-        let filename = this.filename;
-        data.append('file', files[0]);      
+        if (files[0].type == "application/pdf"){
+            window.alert("Conversion to come");
+            this.filename = files[0].name;
+            let filename = this.filename;
+            let src = URL.createObjectURL(files[0]);
+            data.append('file', files[0]);  
 
-        imageWrapper.show(100);
+            imageWrapper.show(100);
+            
+            //var src = (files[0]).toDataURL("image/jpeg", 0.8);
+            //console.log(src);
+            $("#loader").hide();
+            GetyImg('http://127.0.0.1:5154/Image/'+filename);
+        }
+        else{
+            this.filename = files[0].name;
+            let filename = this.filename;
+            data.append('file', files[0]);  
+
+            imageWrapper.show(100);
 
         
-        $.ajax({
-            url: "http://127.0.0.1:5154/Ocr",
-            type: 'put',
-            dataType: 'json',
-            data: data,
-            processData: false,
-            contentType: false,
-            statusCode:{
-                200: function(image){
-                    $("#loader").hide();
-                    setTimeout(()=>{
-                        GetyImg('http://127.0.0.1:5154/Image/'+filename);
-
-                    },150);
+            $.ajax({
+                url: "http://127.0.0.1:5154/Ocr",
+                type: 'put',
+                dataType: 'json',
+                data: data,
+                processData: false,
+                contentType: false,
+                statusCode:{
+                    200: function(image){
+                        $("#loader").hide();
+                        setTimeout(()=>{
+                            GetyImg('http://127.0.0.1:5154/Image/'+filename);
+    
+                        },150);
+                    }
                 }
-            }
+                
+            });
+        }
+
+        // this.filename = files[0].name;
+        // let filename = this.filename;
+        // data.append('file', files[0]);      
+
+        // imageWrapper.show(100);
+
+        
+        // $.ajax({
+        //     url: "http://127.0.0.1:5154/Ocr",
+        //     type: 'put',
+        //     dataType: 'json',
+        //     data: data,
+        //     processData: false,
+        //     contentType: false,
+        //     statusCode:{
+        //         200: function(image){
+        //             $("#loader").hide();
+        //             setTimeout(()=>{
+        //                 GetyImg('http://127.0.0.1:5154/Image/'+filename);
+
+        //             },150);
+        //         }
+        //     }
             
-        });
+        // });
     };
 
     async OpenCamera(){
