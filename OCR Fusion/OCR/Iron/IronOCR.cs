d6d40f6e33d5dbdd46863ce8fed47bf6e6e06783
Louis-Antoine DEVOS <18168@ecam.be>
2023-01-12 15:51:09 +0100

    Region selection

--- OCR Fusion/OCR/Iron/IronOCR.cs
+++ OCR Fusion/OCR/Iron/IronOCR.cs
@@ -2,6 +2,7 @@
 {
     using IronOcr;
     using MongoDB.Bson;
+    using OCR_Fusion.API_Object;
     using System.Text.Json.Nodes;
 
     [Register("IronOCR","Iron","Great recognition of printed caracters")]
@@ -47,18 +48,32 @@
 
         public OutputDefinition GetText(InputDefinition input)
         {
-            IronText(input.imageName);
+            IronText(input);
             output.imageName = input.imageName;
             return output;
         }
 
-        private void IronText(string imageName)
+        private void IronText(InputDefinition input)
         {
             var Ocr = new IronTesseract();
-            using (var Input = new OcrInput())
+            
+                
+            using (var ironInput = new OcrInput())
             {
-                Input.AddImage(@"Uploads\" + imageName);
-                var Result = Ocr.Read(Input);
+                if (input.regions.Count != 0)
+                {
+                    Vector xy1 = input.regions[0][0];
+                    Vector xy2 = input.regions[0][1];
+                    float inputWidth = xy2.x - xy1.x;
+                    float inputHeight = xy2.y - xy1.y;
+                    var ContentArea = new CropRectangle(x: xy1.x, y: xy1.y, width: inputWidth, height: inputHeight);
+                    ironInput.AddImage(@"Uploads\" + input.imageName, ContentArea);
+                }
+                else
+                {
+                    ironInput.AddImage(@"Uploads\" + input.imageName);
+                }
+                var Result = Ocr.Read(ironInput);
                 output.words.Add(Result.Text);
             }
         }
